d09644dc08831a54b07bab0859323bb6da8ea5ba
Gilang Chandrasa <gilang@adlibre.com.au>
2010-11-01 01:28:33 +0000

    add more style to settings

--- media/js/script.js
+++ media/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 	// setup ul.tabs to work as tabs for each div directly under div.panes
 	$("ul.tabs").tabs("div.panes > div");
+	$("#new-rule").click(function() {
+	    $("#new-rule-form").toggle();
+	    return false;
+	});
 });
 
