eac904c8b2e64c7023b407438bcc1ffd7c0a6e21
Andrew Cutler <andrew@adlibre.com.au>
2011-03-21 08:26:52 +0000

    Missed adding this before. Contains custom 500 handler.

---
+++ src/dms/views.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django import http
+from django.template import Context, loader
+
+def server_error(request, template_name='500.html'):
+    """
+    500 error handler.
+
+    Templates: `500.html`
+    Context:
+        MEDIA_URL
+            Path of static media (e.g. "media.example.org")
+    """
+    t = loader.get_template(template_name)
+    return http.HttpResponseServerError(t.render(Context({
+        'MEDIA_URL': settings.MEDIA_URL
+    })))
