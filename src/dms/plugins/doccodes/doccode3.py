e83451ca58ea2ddaa00dc8b315c0a3fedbf0aeb4
Andrew Cutler <andrew@adlibre.com.au>
2011-03-26 09:28:29 +0000

    Added Project Gutenberg eBook name support

---
+++ src/dms/plugins/doccodes/doccode3.py
@@ -0,0 +1,25 @@
+import re
+
+from fileshare.utils import DocCodeProvider
+
+
+class DocCode(DocCodeProvider):
+
+    name = 'Project Gutenberg eBooks'
+    description = '[0-9]{1,6}'
+
+    @staticmethod
+    def validate(document):
+        if re.match('^[0-9]{1,6}$', document):
+            return True
+        else:
+            return False
+
+    # Split document as per Project Gutenberg method for 'eBook number' not, eText
+    # http://www.gutenberg.org/dirs/00README.TXT
+    @staticmethod
+    def split(document):
+        d = []
+        for i in range(len(document)):
+            d.append(document[i-1:i])
+        return d
