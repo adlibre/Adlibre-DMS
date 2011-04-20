c3913a90c2a3a4db09f41e12bb43a48c7e24f291
Andrew Cutler <andrew@adlibre.com.au>
2011-04-20 02:11:12 +0000

    Better way of finding current dir

--- deployment/run_fcgi.sh
+++ deployment/run_fcgi.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # requires flup
 
-CWD="`dirname $0`/";
+CWD=$(cd ${0%/*} && pwd -P)
 
-. ${CWD}../bin/activate
-python ${CWD}../src/dms/manage.py runfcgi protocol=scgi host=127.0.0.1 port=${1}
-
+. ${CWD}/../bin/activate
+python ${CWD}/../src/dms/manage.py runfcgi protocol=scgi host=127.0.0.1 port=${1}
