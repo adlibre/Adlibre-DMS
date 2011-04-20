375d8c9930333b983f6cfaafd0908f65ec8b901e
Andrew Cutler <andrew@adlibre.com.au>
2011-04-20 02:37:39 +0000

    Generic management script for django fcgi projects

---
+++ deployment/manage-fcgi.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Manages the startup and shutdown of this projects FCGI processes.
+# Uses UNIX sockets
+#
+
+# Project Specific Config
+PROJNAME='dms'
+WEB_USER='wwwpub'
+
+CWD=$(cd ${0%/*} && pwd -P)
+PROJDIR=$(cd $CWD/../src/${PROJNAME}/ && pwd -P) # Root of our project source
+BINDIR=$(cd $CWD/../bin/ && pwd -P) # Path to activate / virtualenv
+PIDFILE="$PROJDIR/${PROJNAME}.pid"
+SOCKET="$PROJDIR/${PROJNAME}.sock"
+
+############################################
+
+# Functions
+function startit {
+    echo "Starting ${PROJNAME}:"
+    . ${BINDIR}/activate
+    python ${PROJDIR}/manage.py runfcgi protocol=scgi socket=$SOCKET pidfile=$PIDFILE
+    RC=$?
+}
+
+function stopit {
+    echo "Stopping ${PROJNAME}:"
+
+    if [ -f $PIDFILE ]; then
+        kill `cat -- $PIDFILE` &&
+        rm -f -- $PIDFILE &&
+        RC=$?
+        echo "Process Terminated"
+    else
+        echo "PIDFILE not found. Killing likely processes."
+        kill `pgrep -f "python ${PROJDIR}/manage.py"`
+        exit 128
+    fi
+}
+
+function status {
+    echo "I don't know how to do that yet"
+}
+
+function showUsage {
+    echo "Usage: manage-fcgi.sh {start|stop|status|restart}"
+}
+
+# check that we have required parameters
+if [ "$ACTION" == "" ]; then
+    showUsage
+    exit 128
+fi
+
+# Sanity check username = $WEB_USER else die with error
+if [ ! "`whoami`" == "$WEB_USER" ]; then
+    echo "Error: Must run as ${WEB_USER}."
+    exit 128
+fi
+
+# See how we were called.
+case "$ACTION" in
+    start)
+        startit
+	;;
+    stop)
+        stopit
+	;;
+    status)
+        status
+        ;;
+    restart)
+        stopit
+        startit
+	;;
+    *)
+	    showUsage
+	;;
+esac
+
+exit $RC
