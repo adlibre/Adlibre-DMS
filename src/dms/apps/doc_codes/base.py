9927ee4d522fcf0a2cd1817d8c0b9f6bd816a900
Kate Kondratyeva <kate@adlibre.com.au>
2011-05-21 14:21:08 +0000

    added getters for doccode collection and for doccode title

--- src/dms/apps/doc_codes/base.py
+++ src/dms/apps/doc_codes/base.py
@@ -1,4 +1,9 @@
 class Doccode(object):
     def get_id(self):
         return self.doccode_id
-    
+
+    def get_title(self):
+        title = getattr(self, 'title', '')
+        if not title:
+            title = getattr(self, 'name', '')
+        return title
