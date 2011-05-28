d47e5ccb3081663134d08d4c79ed6e74572dcfba
Kate Kondratyeva <kate@adlibre.com.au>
2011-05-28 09:34:32 +0000

    Reorganized plugins - see #140

--- src/dms/apps/dms_plugins/__init__.py
+++ src/dms/apps/dms_plugins/__init__.py
@@ -1,1 +1,6 @@
-from plugin_list import *
+import pkgutil
+
+import pluginpoints
+
+for module_loader, name, ispkg in pkgutil.walk_packages(__path__, __name__ + "."):
+     module_loader.find_module(name).load_module(name)
--- src/dms/apps/dms_plugins/workers/security/groups.py
+++ src/dms/apps/dms_plugins/workers/security/groups.py
@@ -1,12 +1,26 @@
 from django.contrib.auth.models import Group
 
-from dms_plugins.workers import PluginError
+from dms_plugins.pluginpoints import BeforeStoragePluginPoint, BeforeRetrievalPluginPoint
+from dms_plugins.workers import Plugin, PluginError
 
-class NotInSecurityGroupError(PluginError):
-    def __str__(self):
-        return "NotInSecurityGroupError - You're not in security group"
+class GroupSecurityStore(Plugin, BeforeStoragePluginPoint):
+    name = 'Security Group'
+    description = 'Security group member only'
+    active = True
 
-class GroupSecurityWorker(object):
+    def work(self, request, document):
+        return GroupSecurity().work(request, document)
+
+class GroupSecurityRetrieval(Plugin, BeforeRetrievalPluginPoint):
+    name = 'Security Group'
+    description = 'Security group member only'
+    active = True
+    index = 0
+
+    def work(self, request, document):
+        return GroupSecurity().work(request, document)
+
+class GroupSecurity(object):
     def work(self, request, document, **kwargs):
         security_group, created = Group.objects.get_or_create(name='security')
         if not security_group in request.user.groups.all():
