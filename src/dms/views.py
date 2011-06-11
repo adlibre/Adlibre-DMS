0198d08c554ac1dff659f2e0fe67a6bcbba0766e
Kate Kondratyeva <kate@adlibre.com.au>
2011-06-11 15:22:58 +0000

    using requestContext instead of Context - fixes problems wuth THEME_TEMPLATE

--- src/dms/views.py
+++ src/dms/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django import http
-from django.template import Context, loader
+from django.template import RequestContext, loader
 
 def server_error(request, template_name='500.html'):
     """
@@ -12,6 +12,6 @@ def server_error(request, template_name='500.html'):
             Path of static media (e.g. "media.example.org")
     """
     t = loader.get_template(template_name)
-    return http.HttpResponseServerError(t.render(Context({
+    return http.HttpResponseServerError(t.render(RequestContext({
         'MEDIA_URL': settings.MEDIA_URL
     })))
