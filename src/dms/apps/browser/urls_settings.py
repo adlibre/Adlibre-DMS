ec888a2b83b5b0fa0cdb2e6870be04775407b35f
Kate Kondratyeva <kate@adlibre.com.au>
2011-06-13 06:25:48 +0000

    moved settings views into browser app. Code cleanup.

---
+++ src/dms/apps/browser/urls_settings.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('browser.views',
+    url(r'^$',
+        'setting',
+        name='setting'),
+    url(r'^plugins$',
+        'plugins',
+        name='plugins'),
+    url(r'^(?P<rule_id>\d+)/$',
+        'edit_setting',
+        name='edit_setting'),
+    url(r'^state/(?P<rule_id>\d+)/$',
+        'toggle_rule_state',
+        name='toggle_rule_state'),
+    url(r'^(?P<rule_id>\d+)/conf/(?P<plugin_id>\d+)/$',
+        'plugin_setting',
+        name='plugin_setting'),
+)
