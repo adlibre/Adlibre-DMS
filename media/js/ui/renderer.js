974bb65d5dc57134cfae3dc36eb8aef262225bcd
Kate Kondratyeva <kate@adlibre.com.au>
2011-07-07 11:36:59 +0000

    added document view

--- media/js/ui/renderer.js
+++ media/js/ui/renderer.js
@@ -2,6 +2,7 @@ function UIRenderer(){
     var self = this;
     this.rule_list_id = "ui_rule_list";
     this.document_list_id = "ui_document_list";
+    this.document_container_id = 'ui_document';
 
     this.render_object_list = function(list_id, objects, construct_item_callback){
         var container = $("#" + list_id);
@@ -26,9 +27,16 @@ function UIRenderer(){
         self.render_object_list(self.document_list_id, documents, function(document, document_item){
             var lnk = $('<a>');
             lnk.text(document.name);
-            lnk.attr('href', "#");
+            lnk.attr('href', document.ui_url);
             document_item.append(lnk);
             return document_item;
         });
+    }
+    
+    this.render_document = function(document_url){
+       var iframe = $('<iframe>');
+       iframe.attr('src', document_url);
+       iframe.css('border', '2px solid #333');
+       $('#' + self.document_container_id).empty().append(iframe);
     }
 }
