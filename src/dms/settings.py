768e8e05280ca8d3df296ed0787b945591a34a90
Andrew Cutler <andrew@adlibre.com.au>
2011-11-10 04:55:51 +0000

    Make paths cross platform.

--- src/dms/settings.py
+++ src/dms/settings.py
@@ -21,7 +21,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, '../../db/dms.sqlite'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, '..', '..', 'db', 'dms.sqlite'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -54,9 +54,9 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, '../../media')
-DOCUMENT_ROOT = os.path.join(PROJECT_PATH, '../../documents')
-FIXTURE_DIRS = ( os.path.join(PROJECT_PATH, '../../fixtures'), )
+MEDIA_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'media')
+DOCUMENT_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'documents')
+FIXTURE_DIRS = ( os.path.join(PROJECT_PATH, '..', '..', 'fixtures'), )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
