86cb0b4859e2f55de86c3a80db84f20ded0958aa
Andrew Cutler <andrew@adlibre.com.au>
2012-01-10 07:01:44 +0000

    Moved media to www subdir for cleaner deployment. Tidied up urls.py

--- src/dms/settings.py
+++ src/dms/settings.py
@@ -54,7 +54,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'media')
+MEDIA_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'www', 'media')
 DOCUMENT_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'documents')
 FIXTURE_DIRS = ( os.path.join(PROJECT_PATH, '..', '..', 'fixtures'), )
 
@@ -67,7 +67,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, '../../static')
+STATIC_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'www', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
