033073573c905bb8efae3763fadaa3b43427e66b
Andrew Cutler <andrew@adlibre.com.au>
2012-01-13 07:44:21 +0000

    Some i18n fixups, template cleanups. Added version context var.

--- src/dms/context_processors.py
+++ src/dms/context_processors.py
@@ -10,3 +10,6 @@ def demo(context):
     """ Returns Demo Mode Boolean Context Variable """
     return {'DEMO': settings.DEMO}
 
+def product_version(context):
+    """ Returns Context Variable Containing Product version """
+    return {'PRODUCT_VERSION': settings.PRODUCT_VERSION}
