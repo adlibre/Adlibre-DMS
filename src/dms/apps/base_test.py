4db1f5d0d307aa1b84adc56e787ef704560fd910
Iurii Garmash <yuri@adlibre.com.au>
2012-01-18 19:03:11 +0000

    Closes #592 Tests refactoring Closes #593 Initial data signal

--- src/dms/apps/base_test.py
+++ src/dms/apps/base_test.py
@@ -7,12 +7,12 @@ from django.test import TestCase
 from plugins import models
 
 class AdlibreTestCase(TestCase):
-    fixtures = ['initial_data.json',]
+    fixtures = ['test_initial_data.json',]
     
     def _fixture_setup(self, *args, **kwargs):
         # KLUDGE: dirty hack to have "our" plugins with correct ids, so that mappings had correct plugin relations
-        models.PluginPoint.objects.all().delete()
-        models.Plugin.objects.all().delete()
+        #models.PluginPoint.objects.all().delete()
+        #models.Plugin.objects.all().delete()
         # KLUDGE:  dirty hack ends
         super(AdlibreTestCase, self)._fixture_setup(*args, **kwargs)
         call_command('import_documents', 
