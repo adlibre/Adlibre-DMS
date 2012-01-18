b1ed466f80f0670c5122bc3eb574211b51423af1
Andrew Cutler <andrew@adlibre.com.au>
2012-01-18 06:24:24 +0000

    Cleanup boilerplate

--- src/dms/apps/ui/forms.py
+++ src/dms/apps/ui/forms.py
@@ -1,3 +1,10 @@
+"""
+Module: DMS UI Django Forms
+Project: Adlibre DMS
+Copyright: Adlibre Pty Ltd 2011
+License: See LICENSE for license information
+"""
+
 from django import forms
 from django.contrib.admin import widgets as admin_widgets
 
