96e0f94c7fcf435e1fd5664ea0b4f386a8759a09
Andrew Cutler <andrew@adlibre.com.au>
2012-02-13 11:58:54 +0000

    Cleanup URLS

--- src/dms/apps/browser/urls.py
+++ src/dms/apps/browser/urls.py
@@ -7,22 +7,12 @@ License: See LICENSE for license information
 
 from django.conf.urls.defaults import *
 
-# TODO: Are these regexes optimal?
-
-# DECISION: Need to make adding ruleid option to request url
-# eg /get/{ruleid}/MYDOC.pdf
-
 urlpatterns = patterns('browser.views',
-    url(r'^settings/plugins$',
-        'plugins',
-        name='plugins'),
+    url(r'^$', 'index', name='home'),
+    url(r'^settings/plugins$', 'plugins', name='plugins'),
     url(r'^upload/$', 'upload', name='upload'),
-    url(r'^get/(?P<document>.+)$',
-        'get_file', name='get_file'),
-    url(r'^files/$',
-        'files_index', name='files_index'),
-    url(r'^files/(?P<id_rule>\d+)/$',
-        'files_document', name='files_document'),
-    url(r'^revision/(?P<document>.+)$',
-        'revision_document', name='revision_document'),
+    url(r'^get/(?P<document>.+)$', 'get_file', name='get_file'),
+    url(r'^files/$', 'files_index', name='files_index'),
+    url(r'^files/(?P<id_rule>\d+)/$', 'files_document', name='files_document'),
+    url(r'^revision/(?P<document>.+)$', 'revision_document', name='revision_document'),
 )
