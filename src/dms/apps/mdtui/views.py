c9091474a301571a93129412946a67ca3a2ef0bf
Andrew Cutler <andrew@adlibre.com.au>
2012-02-14 03:58:59 +0000

    Using Twitter Bootstrap now for MDTUI

--- src/dms/apps/mdtui/views.py
+++ src/dms/apps/mdtui/views.py
@@ -1,22 +1,21 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.admin.views.decorators import staff_member_required
-from django.core.urlresolvers import reverse, resolve
-from django.shortcuts import render_to_response, HttpResponseRedirect, get_object_or_404
-from django.http import HttpRequest, HttpResponse, Http404
-from django.views.generic.simple import direct_to_template
-from django.template import RequestContext, loader
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.sessions.models import Session
-from django.contrib.auth.views import login as login_base
-from django.contrib.auth.models import User
+"""
+Module: MDT UI Views
+Project: Adlibre DMS
+Copyright: Adlibre Pty Ltd 2012
+License: See LICENSE for license information
+"""
 
-def retrieve(request, step, template = 'retrieve.html'):
+from django.shortcuts import render_to_response, HttpResponseRedirect, get_object_or_404
+from django.template import RequestContext, loader
+
+
+def retrieve(request, step=None, template='retrieve.html'):
 
     context = { 'step': step, }
     return render_to_response(template, context, context_instance=RequestContext(request))
 
 
-def upload(request, step, template = 'upload.html'):
+def upload(request, step=None, template='upload.html'):
 
     context = { 'step': step, }
     return render_to_response(template, context, context_instance=RequestContext(request))
