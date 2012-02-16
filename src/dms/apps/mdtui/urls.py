e5fba9bf5e98da0509910a32b6ae2100eca95ed3
Andrew Cutler <andrew@adlibre.com.au>
2012-02-16 02:31:32 +0000

    Added warnings / error example. Added sexy background images. Got rid of some start pages. Refs #603

--- src/dms/apps/mdtui/urls.py
+++ src/dms/apps/mdtui/urls.py
@@ -13,11 +13,12 @@ import mdtui.views
 
 urlpatterns = patterns('mdtui.views',
     url(r'^$', direct_to_template, {'template': 'home.html'}, name='mdtui-home'),
-    url(r'^search/$', 'search', name='mdtui-search'),
+
+    url(r'^search/$', 'search', {'step':'1',}, name='mdtui-search'),
     url(r'^search/options$', 'search', {'step':'1',}, name='mdtui-search-1'),
     url(r'^search/results$', 'search', {'step':'2',},name='mdtui-search-2'),
 
-    url(r'^indexing/$', 'indexing', name='mdtui-index'),
+    url(r'^indexing/$', 'indexing', {'step':'1',}, name='mdtui-index'),
     url(r'^indexing/1$', 'indexing', {'step':'1',}, name='mdtui-index-1'),
     url(r'^indexing/2$', 'indexing', {'step':'2',}, name='mdtui-index-2'),
     url(r'^indexing/3$', 'indexing', {'step':'3',}, name='mdtui-index-3'),
