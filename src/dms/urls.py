5c455214771d910722c383be8d204c41fcfbfa44
Andrew Cutler <andrew@adlibre.com.au>
2012-02-16 07:25:28 +0000

    Cleanup and added bcp Refs #600

--- src/dms/urls.py
+++ src/dms/urls.py
@@ -12,31 +12,30 @@ urlpatterns = []
 
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
-        (r'^500/$', 'dms.views.server_error'),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+        url(r'^500/$', 'dms.views.server_error'),
     )
 
 urlpatterns += patterns('',
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^settings/', include('browser.urls_settings')),
     url(r'^docs/', include('docs.urls')),
-    url(r'^ui/', include('ui.urls')),
-    url(r'^mdtui/', include('mdtui.urls')),
-
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 
     # robots
-    (r'^robots.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype' : 'text/plain'}),
+    url(r'^robots.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype' : 'text/plain'}),
     # favicon
-    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to', {'url': settings.MEDIA_URL+'favicon.ico'}),
+    url(r'^favicon.ico$', 'django.views.generic.simple.redirect_to', {'url': settings.MEDIA_URL+'favicon.ico'}),
+
+    # Independent DMS Apps
+    url(r'^ui/', include('ui.urls')),
+    url(r'^mdtui/', include('mdtui.urls')),
+    url(r'^bcp/', include('bcp.urls')),
+
 
     # This needs to be last
-    (r'^', include('browser.urls')),
+    url(r'^', include('browser.urls')),
 )
 
