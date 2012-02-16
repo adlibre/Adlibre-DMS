72ce05f6a2832e1b40a377ae71de06fa5de2b2e2
Andrew Cutler <andrew@adlibre.com.au>
2012-02-16 07:24:58 +0000

    Added Barcode Printer app. Refs #600

---
+++ src/dms/apps/bcp/urls.py
@@ -0,0 +1,16 @@
+"""
+Module: Barcode Printer URLS
+Project: Adlibre DMS
+Copyright: Adlibre Pty Ltd 2012
+License: See LICENSE for license information
+"""
+
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import redirect_to
+
+import mdtui.views
+
+urlpatterns = patterns('bcp.views',
+    url(r'^(?P<barcode_type>[\d\w]+)/(?P<code>[\d\w]+)$', 'generate', name='bcp-generate'),
+)
