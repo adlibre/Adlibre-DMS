cfa92bac7bd132c57874650409b60ffe81a0902c
Iurii Garmash <yuri@adlibre.com.au>
2012-02-17 12:06:01 +0000

    Refs #629 Plugin has manager, Tags now saved into CouchDB too. (Sync to Django DB appears on plugin's doc save to update DMS internal tags in CouchDB)

--- src/dms/apps/dms_plugins/workers/database/couchdb.py
+++ src/dms/apps/dms_plugins/workers/database/couchdb.py
@@ -1,56 +1,15 @@
-import datetime
-import json
-import os
-
-from django.conf import settings
-
 from dms_plugins.pluginpoints import BeforeRetrievalPluginPoint, BeforeRemovalPluginPoint, DatabaseStoragePluginPoint
+from dms_plugins.models import Document as DocTags #needs refactoring of name
 from dms_plugins.workers import Plugin, PluginError, BreakPluginChain
 from document_manager import DocumentManager
+from couch_manager import CouchDocument
 
-
-from datetime import datetime
-from django.db import models
-
-from couchdbkit.ext.django.schema import *
-
-class CouchDocument(Document):
-    id = StringProperty()
-    metadata_doc_type_rule_id = StringProperty(default="")
-    metadata_user_id = StringProperty(default="")
-    metadata_user_name = StringProperty(default="")
-    metadata_created_date = DateTimeProperty(default=datetime.utcnow)
-    metadata_description = StringProperty(default="")
-    tags = ListProperty(default=[])
-    mdt_indexes = DictProperty(default={})
-    search_keywords = ListProperty(default=[])
-    revisions = DictProperty(default={})
-
-    class Meta:
-        app_label = "dmscouch"
-
-    def populate_from_dms(self, request, document):
-        self.id = document.file_name
-        self.metadata_doc_type_rule_id = str(document.doccode.doccode_id)
-        self.metadata_user_id = str(request.user.pk)
-        if request.user.first_name:
-            self.metadata_user_name = request.user.first_name + u'' + request.user.last_name
-        else:
-            self.metadata_user_name = request.user.username
-        #self.metadata_created_date = document.metadata[document.revision]["created_date"]
-        #self.metadata_description = document.db_info["description"] or "" # not implemented yet
-        self.tags = document.tags
-        self.mdt_indexes = {} # not implemented yet
-        self.search_keywords = [] # not implemented yet
-        self.revisions = document.metadata
 
 class CouchDBMetadata(object):
     """
         Stores metadata in CouchDB DatabaseManager.
         Handles required logic for metadata <> Document(object) manipulations.
     """
-#    def __init__(self):
-#        self.database = DatabaseManager()
 
     def store(self, request, document):
         # doing nothing for no doccode documents
@@ -60,11 +19,13 @@ class CouchDBMetadata(object):
         if not document.metadata:
             manager = DocumentManager()
             document = manager.retrieve(request, document.file_name, only_metadata=True)
-        # assuming no document exists
+        # updating tags to sync with Django DB
+        self.sync_document_tags(document)
+        # assuming no document with this _id exists. SAVING
         couchdoc=CouchDocument()
         couchdoc.populate_from_dms(request, document)
-        couchdoc.save()
-        print "Setoring Document into DB", document
+        couchdoc.save(force_update=True)
+        print "Storing Document into DB", document
         return document
 
     def update_metadata_after_removal(self, request, document):
@@ -74,6 +35,19 @@ class CouchDBMetadata(object):
         print "Getting Document from DB", document
         return document
 
+    """
+    Helper managers:
+    """
+    def sync_document_tags(self, document):
+        if not document.tags:
+            tags = []
+            try:
+                doc_model = DocTags.objects.get(name = document.get_stripped_filename())
+                tags = doc_model.get_tag_list()
+            except DocTags.DoesNotExist:
+                pass
+            document.set_tags(tags)
+        return document.tags
 
 class CouchDBMetadataRetrievalPlugin(Plugin, BeforeRetrievalPluginPoint):
     title = "CocuhDB Metadata Retrieval"
