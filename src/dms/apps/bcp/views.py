9e2786dee7082266f82abb923b25ddfa720789ec
Andrew Cutler <andrew@adlibre.com.au>
2012-02-17 03:22:01 +0000

    Remove magic. Now using class factory for selecting barcode type.
    Refs: #600

--- src/dms/apps/bcp/views.py
+++ src/dms/apps/bcp/views.py
@@ -5,13 +5,20 @@ Copyright: Adlibre Pty Ltd 2012
 License: See LICENSE for license information
 """
 
+# NB FIXME: Same error? http://code.google.com/p/elaphe/issues/detail?id=9
+
+from elaphe import barcode
+
 from django.http import HttpResponse
 from django.http import Http404, HttpResponseBadRequest
+
+from django.conf import settings
 
 try:
     from cStringIO import StringIO
 except ImportError:
     from StringIO import StringIO
+
 
 def generate(request, barcode_type, code):
     """
@@ -21,19 +28,17 @@ def generate(request, barcode_type, code):
     response['Content-Disposition'] = 'inline; filename=%s.pdf' % (code,)
 
     try:
-        # dynamically load the barcode type module, or raise 404
-        exec('from elaphe.upc import %s as barcode' % barcode_type)
-    except ImportError:
-        raise Http404
-
-    bc = barcode()
-
-    bc_image = bc.render(str(code), options=dict(includetext=True), scale=4, margin=1)
+        bc = barcode(barcode_type, str(code), options=dict(includetext=True), scale=4, margin=1)
+    except ValueError, e:
+        return HttpResponseBadRequest('Barcode Generation Failed: %s' % (e))
     buffer = StringIO()
     try:
-        bc_image.save(buffer, 'pdf')
-    except IOError:
-        return HttpResponseBadRequest('Barcode Generation Failed')
+        bc.save(buffer, 'pdf')
+    except IOError, e:
+        if settings.DEBUG:
+            raise
+        else:
+            return HttpResponseBadRequest('Barcode Generation Failed: %s' % (e))
 
     # Get the value of the StringIO buffer and write it to the response.
     pdf = buffer.getvalue()
