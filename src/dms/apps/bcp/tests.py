97ae4eeb2be9e0a7126d247eb6a9bb6a2b2cf308
Andrew Cutler <andrew@adlibre.com.au>
2012-02-23 06:23:15 +0000

    Improvements to test data structure Refs #600

--- src/dms/apps/bcp/tests.py
+++ src/dms/apps/bcp/tests.py
@@ -16,14 +16,17 @@ Test data
 username = 'admin'
 password = 'admin'
 
-variable_length_barcode_types = ('code39',)
-variable_length_barcode_codes = ('ABC1234', '123456789', 'ABC-1234')
+barcode_test_data = [
+  # ('type', 'barcode'),
+    ('code39', 'ABC1234'),
+    ('code39', 'ABC-1234'),
+    ('code39', '123456789'),
+]
 
 class BarCodeTest(AdlibreTestCase):
 
     def test_generate_barcode(self):
-        for type in variable_length_barcode_types:
-            for code in variable_length_barcode_codes:
-                url = reverse('bcp-generate', kwargs = {'barcode_type': type, 'code': code, })
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, 200)
+        for barcode in barcode_test_data:
+            url = reverse('bcp-generate', kwargs = {'barcode_type': barcode[0], 'code': barcode[1], })
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
