97d712d9caa6baa52f563b82554aecedce1cf2c2
Iurii Garmash <yuri@adlibre.com.au>
2012-03-06 12:02:39 +0000

    Refs #659 MDTUI Polishing + tests.  Views simplified. Step 1 Form stores DocRule Selected for returning backwards steps.

--- src/dms/apps/mdtui/views.py
+++ src/dms/apps/mdtui/views.py
@@ -4,18 +4,19 @@ Project: Adlibre DMS
 Copyright: Adlibre Pty Ltd 2012
 License: See LICENSE for license information
 """
-import datetime
 
 from django.core.urlresolvers import reverse, resolve
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, HttpResponseRedirect, get_object_or_404
 from django.template import RequestContext, loader
+from django.contrib.auth.decorators import login_required
 
 from dmscouch.models import CouchDocument
 
-from forms import DocumentIndexForm, DocumentTypeSelectForm, DocumentUploadForm
-from forms_representator import get_mdts_for_docrule, render_fields_from_docrules
+from forms import DocumentTypeSelectForm, DocumentUploadForm
+
 from document_manager import DocumentManager
+from view_helpers import *
 
 
 MDTUI_ERROR_STRINGS = {
@@ -25,7 +26,7 @@ MDTUI_ERROR_STRINGS = {
     4:'{"status": "Error. You have not defined Document Type Rule. Search form can not be created."}'
 }
 
-
+@login_required
 def search_type(request, step, template='mdtui/search.html'):
     context = { 'step': step, }
     form = DocumentTypeSelectForm(request.POST or None)
@@ -35,7 +36,6 @@ def search_type(request, step, template='mdtui/search.html'):
                     docrule = form.data["docrule"]
                 except:
                     return HttpResponse(MDTUI_ERROR_STRINGS[1])
-                    # TODO: refactor this (unright but quick)
                 request.session['docrule'] = docrule
                 return HttpResponseRedirect(reverse('mdtui-search-options'))
             else:
@@ -47,7 +47,7 @@ def search_type(request, step, template='mdtui/search.html'):
     context.update({ 'form': form, })
     return render_to_response(template, context, context_instance=RequestContext(request))
 
-
+@login_required
 def search_options(request, step, template='mdtui/search.html'):
     context = { 'step': step, }
     try:
@@ -76,7 +76,7 @@ def search_options(request, step, template='mdtui/search.html'):
     context.update({ 'form': form, })
     return render_to_response(template, context, context_instance=RequestContext(request))
 
-
+@login_required
 def search_results(request, step=None, template='mdtui/search.html'):
     document_keys = None
     docrule_id = None
@@ -112,7 +112,7 @@ def search_results(request, step=None, template='mdtui/search.html'):
                 }
     return render_to_response(template, context, context_instance=RequestContext(request))
 
-
+@login_required
 def search_viewer(request, code, step, template='mdtui/search.html'):
     context = { 'step': step,
                 'code': code,
@@ -120,10 +120,11 @@ def search_viewer(request, code, step, template='mdtui/search.html'):
     return render_to_response(template, context, context_instance=RequestContext(request))
 
 
-
+@login_required
 def indexing(request, step=None, template='mdtui/indexing.html'):
     # Context init
     context = {}
+    docrule = None
     document_keys = None
     form = DocumentTypeSelectForm()
 
@@ -142,7 +143,6 @@ def indexing(request, step=None, template='mdtui/indexing.html'):
                     docrule = form.data["docrule"]
                 except:
                     return HttpResponse(MDTUI_ERROR_STRINGS[1])
-                # TODO: refactor this (unright but quick)
                 request.session['current_step'] = step
                 request.session['docrule_id'] = docrule
                 step = str(int(step) + 1)
@@ -159,7 +159,15 @@ def indexing(request, step=None, template='mdtui/indexing.html'):
             return HttpResponseRedirect(reverse('mdtui-index-' + step))
     else:
         if step == "1":
+            # form initing with docrule set if it was done previous
+            try:
+                docrule = request.session["docrule_id"]
+            except:
+                pass
             form = DocumentTypeSelectForm()
+            # HACK to set form data to previously set up if returning back to this page from later steps
+            if docrule:
+                form.base_fields["docrule"].initial = form.fields["docrule"].choices[int(docrule)]
         if step == "2":
             try:
                 docrule = request.session['docrule_id']
@@ -180,6 +188,7 @@ def indexing(request, step=None, template='mdtui/indexing.html'):
                     })
     return render_to_response(template, context, context_instance=RequestContext(request))
 
+@login_required
 def uploading(request, step=None, template='mdtui/indexing.html'):
     document_keys = None
     context = {}
@@ -212,102 +221,10 @@ def uploading(request, step=None, template='mdtui/indexing.html'):
             del request.session["document_keys_dict"]
     return render_to_response(template, context, context_instance=RequestContext(request))
 
+
 def barcode(request):
     return HttpResponse('Barcode Printing')
 
-def initDocumentIndexForm(request):
-        """
-        DocumentIndexForm initialization
-        in case of GET returns an empty base form,
-        in case of POST returns populated (from request) form instance.
-        in both cases form is rendered with MDT index fields
-        """
-        try:
-            details = get_mdts_for_docrule(request.session['docrule_id'])
-        except KeyError:
-            details = get_mdts_for_docrule(request.session['docrule'])
 
-        form = DocumentIndexForm()
-        if not details == 'error':
-            # MDT's exist for ths docrule adding fields to form
-            fields = render_fields_from_docrules(details)
-            #print fields
-            if fields:
-                form.setFields(fields)
-        if request.POST:
-            form.setData(request.POST)
-        return form
 
-def processDocumentIndexForm(request):
-        form = initDocumentIndexForm(request)
-        secondary_indexes = {}
-        search = None
-        try:
-            search = request.session["docrule"]
-        except: pass
-        if form.validation_ok() or search:
-            for key, field in form.fields.iteritems():
-                try:
-                    # for native form fields
-                    secondary_indexes[field.field_name] = form.data[unicode(key)]
-                except:
-                    # for dynamical form fields
-                    secondary_indexes[key] = form.data[unicode(key)]
-            #print secondary_indexes
-            if secondary_indexes:
-                return secondary_indexes
-            else:
-                return None
 
-def convert_search_res(search_res, match_len):
-    docs_list = {}
-    matched_docs = []
-    for row in search_res:
-        if row.get_id in docs_list.keys():
-            docs_list[row.get_id] += 1
-        else:
-            docs_list[row.get_id] = 1
-    for doc_id, mention_count in docs_list.iteritems():
-        if int(mention_count) >= int(match_len):
-            matched_docs.append(doc_id)
-    return matched_docs
-
-def convert_to_search_keys(document_keys, docrule_id):
-    req_params = []
-    for key, value in document_keys.iteritems():
-        if key != "date":
-            if not "date" in document_keys.keys():
-                req_params.append([key, value, docrule_id],)
-            else:
-                req_params.append([key, value, docrule_id, str_date_to_couch(document_keys["date"])],)
-    return req_params
-
-def cleanup_document_keys(document_keys):
-    # cleaning up key/value pairs that have empty values from couchdb search request
-    del_list = []
-    for key, value in document_keys.iteritems():
-        if not value:
-            del_list.append(key)
-    for key in del_list:
-        del document_keys[key]
-    return document_keys
-
-def str_date_to_couch(from_date):
-    """
-    Converts date from form date widget generated format, like '2012-03-02'
-    To CouchDocument stored date. E.g.: '2012-03-02T00:00:00Z'
-    """
-    # TODO: HACK: normal datetime conversions here
-    couch_date = from_date + 'T00:00:00Z'
-#    date = datetime.datetime.strptime(from_date, "%Y-%m-%d")
-#    couch_date = datetime.datetime.now()
-    return couch_date
-
-def get_mdts_for_documents(documents):
-    indexes = {}
-    if documents:
-        for document in documents:
-            xes = document.mdt_indexes
-            for ind in xes:
-                indexes[ind] = "index"
-    return indexes.keys()
