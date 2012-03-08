0f6ccc543fe3f63a2f9150612330575891d0dcd7
Iurii Garmash <yuri@adlibre.com.au>
2012-03-08 18:51:14 +0000

    Closes #659 MDTUI Tests working and cleaning up after themselves.

---
+++ src/dms/couchapps/parallell_keys.py
@@ -0,0 +1,10 @@
+"""
+Module: DMS Parallell Keys manager
+Project: Adlibre DMS
+Copyright: Adlibre Pty Ltd 2012
+License: See LICENSE for license information
+Author: Iurii Garmash
+"""
+
+from django.conf import settings
+from mdtcouch.models import MetaDataTemplate
