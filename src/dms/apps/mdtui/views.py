9df248b91d426e9a142ea54802f50279873bf8a4
Iurii Garmash <yuri@adlibre.com.au>
2012-03-08 17:31:00 +0000

    Closes #650 MDTUI Warnings handling. Proper content warnings rendered instead of error JSON's.

--- src/dms/apps/mdtui/views.py
+++ src/dms/apps/mdtui/views.py
@@ -20,40 +20,46 @@ from view_helpers import *
 
 
 MDTUI_ERROR_STRINGS = {
-    1:'{"status": "Error. You have not selected Doccument Type Rule."}',
-    2:'{"status": "Error. You have not entered Document Indexing Data."}',
-    3:'{"status": "Error. You have not defined Document Searching Keys."}',
-    4:'{"status": "Error. You have not defined Document Type Rule. Search form can not be created."}'
+    1:'You have not selected Doccument Type Rule.',
+    2:'You have not entered Document Indexing Data. Document will not be searchable by indexes.',
+    3:'You have not defined Document Searching Options.',
+    4:'You have not defined Document Type Rule. Can only search by "Creation Date".',
 }
 
 @login_required
 def search_type(request, step, template='mdtui/search.html'):
-    context = { 'step': step, }
+    docrule = None
     form = DocumentTypeSelectForm(request.POST or None)
     if request.POST:
             if form.is_valid():
-                try:
-                    docrule = form.data["docrule"]
-                except:
-                    return HttpResponse(MDTUI_ERROR_STRINGS[1])
+                docrule = form.data["docrule"]
                 request.session['docrule'] = docrule
                 return HttpResponseRedirect(reverse('mdtui-search-options'))
             else:
-                pass
-                # TODO: return form on current step with errors
+                return HttpResponse(MDTUI_ERROR_STRINGS[1])
     else:
         form = DocumentTypeSelectForm()
+        # Trying to set docrule if previously selected
+        try:
+            docrule = request.session['docrule']
+        except KeyError:
+            pass
+        if docrule:
+            form = DocumentTypeSelectForm({'docrule': docrule})
 
-    context.update({ 'form': form, })
+    context = {
+                'step': step,
+                'form': form,
+               }
     return render_to_response(template, context, context_instance=RequestContext(request))
 
 @login_required
 def search_options(request, step, template='mdtui/search.html'):
-    context = { 'step': step, }
+    warnings = []
     try:
         docrule = request.session["docrule"]
     except KeyError:
-        return HttpResponse(MDTUI_ERROR_STRINGS[4])
+        warnings.append(MDTUI_ERROR_STRINGS[4])
 
     form = initDocumentIndexForm(request)
     # excluding description from search form
@@ -73,7 +79,11 @@ def search_options(request, step, template='mdtui/search.html'):
         # date field should be empty
         form.fields["date"].initial = None
 
-    context.update({ 'form': form, })
+    context = {
+                'form': form,
+                'warnings': warnings,
+                'step': step,
+               }
     return render_to_response(template, context, context_instance=RequestContext(request))
 
 @login_required
@@ -82,33 +92,37 @@ def search_results(request, step=None, template='mdtui/search.html'):
     docrule_id = None
     documents = None
     search_res = None
+    warnings = []
+    mdts_list = None
     try:
         document_keys = request.session["document_search_dict"]
         docrule_id = request.session['docrule']
     except KeyError:
-        return HttpResponse(MDTUI_ERROR_STRINGS[3])
+        warnings.append(MDTUI_ERROR_STRINGS[3])
 
-    # turning document_search dict into something useful for the couch request
-    cleaned_document_keys  = cleanup_document_keys(document_keys)
-    if "date" in cleaned_document_keys.keys() and cleaned_document_keys.__len__() == 1:
-        # only one crytheria 'date' in search request, requesting another view
-        #print 'searching only by date: ', str_date_to_couch(cleaned_document_keys["date"]), ' docrule:', docrule_id
-        documents = CouchDocument.view('dmscouch/search_date', key=[str_date_to_couch(cleaned_document_keys["date"]), docrule_id], include_docs=True )
-    else:
-        #print 'multiple keys search'
-        couch_req_params = convert_to_search_keys(cleaned_document_keys, docrule_id)
-        if couch_req_params:
-            search_res = CouchDocument.view('dmscouch/search', keys=couch_req_params )
-            # docuents now returns ANY search type results.
-            # we need to convert it to ALL
-            docs_list = convert_search_res(search_res, couch_req_params.__len__())
-            documents = CouchDocument.view('_all_docs', keys=docs_list, include_docs=True )
-    mdts_list = get_mdts_for_documents(documents)
+    if document_keys:
+        # turning document_search dict into something useful for the couch request
+        cleaned_document_keys  = cleanup_document_keys(document_keys)
+        if "date" in cleaned_document_keys.keys() and cleaned_document_keys.__len__() == 1:
+            # only one crytheria 'date' in search request, requesting another view
+            #print 'searching only by date: ', str_date_to_couch(cleaned_document_keys["date"]), ' docrule:', docrule_id
+            documents = CouchDocument.view('dmscouch/search_date', key=[str_date_to_couch(cleaned_document_keys["date"]), docrule_id], include_docs=True )
+        else:
+            #print 'multiple keys search'
+            couch_req_params = convert_to_search_keys(cleaned_document_keys, docrule_id)
+            if couch_req_params:
+                search_res = CouchDocument.view('dmscouch/search', keys=couch_req_params )
+                # docuents now returns ANY search type results.
+                # we need to convert it to ALL
+                docs_list = convert_search_res(search_res, couch_req_params.__len__())
+                documents = CouchDocument.view('_all_docs', keys=docs_list, include_docs=True )
+        mdts_list = get_mdts_for_documents(documents)
 
     context = { 'step': step,
                 'documents': documents,
                 'document_keys': document_keys,
                 'mdts': mdts_list,
+                'warnings': warnings,
                 }
     return render_to_response(template, context, context_instance=RequestContext(request))
 
@@ -126,6 +140,7 @@ def indexing(request, step=None, template='mdtui/indexing.html'):
     context = {}
     docrule = None
     document_keys = None
+    warnings = []
     form = DocumentTypeSelectForm()
 
     try:
@@ -165,17 +180,15 @@ def indexing(request, step=None, template='mdtui/indexing.html'):
             except:
                 pass
             form = DocumentTypeSelectForm()
-            # HACK to set form data to previously set up if returning back to this page from later steps
             if docrule:
-                form.base_fields["docrule"].initial = form.fields["docrule"].choices[int(docrule)]
+                form = DocumentTypeSelectForm({'docrule': docrule})
         if step == "2":
             try:
                 docrule = request.session['docrule_id']
             except KeyError:
                 # This error only should appear if something breaks
-                return HttpResponse(MDTUI_ERROR_STRINGS[1])
-            if not request.POST:
-                form = initDocumentIndexForm(request)
+                warnings.append(MDTUI_ERROR_STRINGS[1])
+            form = initDocumentIndexForm(request)
 
     try:
         document_keys = request.session["document_keys_dict"]
@@ -185,6 +198,7 @@ def indexing(request, step=None, template='mdtui/indexing.html'):
     context.update( { 'step': step,
                       'form': form,
                       'document_keys': document_keys,
+                      'warnings': warnings,
                     })
     return render_to_response(template, context, context_instance=RequestContext(request))
 
@@ -192,10 +206,11 @@ def indexing(request, step=None, template='mdtui/indexing.html'):
 def uploading(request, step=None, template='mdtui/indexing.html'):
     document_keys = None
     context = {}
+    warnings = []
     try:
         document_keys = request.session["document_keys_dict"]
     except KeyError:
-        return HttpResponse(MDTUI_ERROR_STRINGS[2])
+        warnings.append(MDTUI_ERROR_STRINGS[2])
     form = DocumentUploadForm(request.POST or None, request.FILES or None)
     if step == "3":
         if request.POST:
@@ -211,6 +226,7 @@ def uploading(request, step=None, template='mdtui/indexing.html'):
     context.update( { 'step': step,
                       'form': form,
                       'document_keys': document_keys,
+                      'warnings': warnings,
                     })
     if step == "4":
         if request.POST:
