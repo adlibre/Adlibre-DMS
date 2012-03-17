aa3e5facf6e22a04f0a76cc1f2154dca01cd9358
Andrew Cutler <andrew@adlibre.com.au>
2012-03-17 06:43:12 +0000

    Disable flatpage middleware, now that we're not using it

--- src/dms/settings.py
+++ src/dms/settings.py
@@ -133,7 +133,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+#    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'dms.urls'
