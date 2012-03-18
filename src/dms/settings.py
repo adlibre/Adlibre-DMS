914782260997402129e7e42c4888f171c63ade04
Andrew Cutler <andrew@adlibre.com.au>
2012-03-18 03:31:27 +0000

    Added application logging config. Added logging to api. Created log directory Refs #636

--- src/dms/settings.py
+++ src/dms/settings.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-import socket
+
 
 PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
 sys.path.append(os.path.join(PROJECT_PATH, 'apps'))
@@ -191,18 +191,64 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '[%(levelname)s] %(name)s %(message)s'
+        },
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s %(message)s'
+        },
+    },
     'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': os.path.join(PROJECT_PATH, '..', '..', 'log', 'dms.log'),
+            'when': 'midnight',
+            'interval': 1, # 1 day
+            'backupCount': 14, # two weeks
+            'formatter': 'verbose',
+        },
         'mail_admins': {
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
         }
     },
     'loggers': {
+        # Filter out restkit to null
+        'restkit': {
+            'handlers': ['null',],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        # Default logger
+        '': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        # API logger
+        'dms.api': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        # Django 500 error logger
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
         },
+
     }
 }
 
