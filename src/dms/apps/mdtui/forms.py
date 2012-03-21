adb99221a49f5bffa27d5097f20ad864066c98ba
Iurii Garmash <yuri@adlibre.com.au>
2012-03-21 17:16:27 +0000

    Closes #663 MUI indexing form complex validation

--- src/dms/apps/mdtui/forms.py
+++ src/dms/apps/mdtui/forms.py
@@ -12,6 +12,11 @@ import datetime
 from dms_plugins.models import DOCRULE_CHOICES
 from django.core.exceptions import ValidationError
 
+CUSTOM_ERRORS = {
+    0: 'Must be Number. (example "123456")',
+    1: 'Must be in valid Date format. (example "2012-12-31")'
+}
+
 class DocumentTypeSelectForm(forms.Form):
     docrule = forms.ChoiceField(choices = DOCRULE_CHOICES, label="Document Type")
 
@@ -68,19 +73,52 @@ class DocumentIndexForm(forms.Form):
     def validation_ok(self):
         """
         Form validation sequence overridden here.
+        Does check if field is entered (basic fields validation)
+        Does Type validation for the proper data entry.
+        E.g. if user enters ABC instead of 123 or date in wrong format.
+        Char fields are only checked if entered at all.
         """
         for field in self.fields:
             cur_field = self.fields[field]
-            #print cur_field
+            # Simple if field entered validation
             try:
-                cleaned_value = cur_field.validate(self.data[unicode(field)])
-            except Exception, e:
-                #print e
-                # appending error to form errors
-                self.errors[field] = e
-                self._errors[field] = e
+                # Validate only if those keys exist e.g in search usage there is no description field
+                try:
+                    cleaned_value = cur_field.validate(self.data[unicode(field)])
+                except ValidationError, e:
+                    # appending error to form errors
+                    self.errors[field] = e
+                    self._errors[field] = e
+            except KeyError:
+                pass
 
-            # TODO: dynamic data (type based validation here)
+            # Wrong type validation
+            try:
+                # Validate only if those keys exist
+                if cur_field.__class__.__name__ == "IntegerField":
+                    try:
+                        int(self.data[unicode(field)])
+                    except ValueError:
+                        # appending error to form errors
+                        if self.data[unicode(field)]:
+                            # Wrong data entered adding type error
+                            e = ValidationError(CUSTOM_ERRORS[0])
+                            self.errors[field] = e
+                            self._errors[field] = e
+                        pass
+                if cur_field.__class__.__name__ == "DateField":
+                    try:
+                        datetime.datetime.strptime(self.data[unicode(field)], "%Y-%m-%d")
+                    except ValueError:
+                        # appending error to form errors
+                        if self.data[unicode(field)]:
+                            # Wrong data entered adding type error
+                            e = ValidationError(CUSTOM_ERRORS[1])
+                            self.errors[field] = e
+                            self._errors[field] = e
+                        pass
+            except KeyError:
+                pass
         if self.errors:
             return False
         return True
