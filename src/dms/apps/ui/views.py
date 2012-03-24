aeaffc0f1d3794408d53e7f2f3b83e6049b587eb
Andrew Cutler <andrew@adlibre.com.au>
2012-03-24 00:36:45 +0000

    Fix bug with suggested_format == ''. PEP8

--- src/dms/apps/ui/views.py
+++ src/dms/apps/ui/views.py
@@ -16,16 +16,18 @@ from django.contrib.auth.decorators import login_required
 from ui.forms import CalendarForm
 from api.handlers import FileHandler
 
-def get_urls(id_rule = None, document_name = None):
-    c = {   'rules_url': reverse('api_rules', kwargs = {'emitter_format': 'json'}),}
+def get_urls(id_rule=None, document_name=None):
+    c = {   'rules_url': reverse('api_rules', kwargs={'emitter_format': 'json'}),}
     if id_rule:
         c.update({
-            'documents_url': reverse("api_file_list", kwargs = {'id_rule': id_rule}),
-            'tags_url': reverse('api_tags', kwargs = {'emitter_format': 'json', 'id_rule': id_rule}),
+            'documents_url': reverse("api_file_list", kwargs={'id_rule': id_rule}),
+            'tags_url': reverse('api_tags', kwargs={'emitter_format': 'json', 'id_rule': id_rule}),
             })
     if document_name:
         code, suggested_format = os.path.splitext(document_name)
         suggested_format = suggested_format[1:] # Remove . from file ext
+        if suggested_format == '':
+            suggested_format = None
         c.update({  'document_url': reverse('api_file', kwargs={'code': code,'suggested_format': suggested_format,}),
                     'document_info_url': reverse('api_file', kwargs={'code': code,'suggested_format': suggested_format,}),
                     })
@@ -40,7 +42,7 @@ def rule_list(request):
 @login_required
 def document_list(request, id_rule):
     template_name = "ui/document_list.html"
-    c = {'communicator_options': get_urls(id_rule = id_rule),
+    c = {'communicator_options': get_urls(id_rule=id_rule),
         'rule_id': id_rule,
         'calendar_form': CalendarForm()}
     return direct_to_template(request, template_name, c)
@@ -48,7 +50,7 @@ def document_list(request, id_rule):
 @login_required
 def document(request, document_name):
     template_name = "ui/document.html"
-    c = {'communicator_options': get_urls(document_name = document_name),
+    c = {'communicator_options': get_urls(document_name=document_name),
         'document_name': document_name}
     return direct_to_template(request, template_name, c)
 
