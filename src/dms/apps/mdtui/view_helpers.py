643464b851a361865fe24e27038610d754298245
Iurii Garmash <yuri@adlibre.com.au>
2012-03-30 13:30:12 +0000

    Refs #686 MUI range search (massive views refactor)

--- src/dms/apps/mdtui/view_helpers.py
+++ src/dms/apps/mdtui/view_helpers.py
@@ -7,7 +7,8 @@ Author: Iurii Garmash
 """
 
 from forms import DocumentIndexForm
-from forms_representator import get_mdts_for_docrule, render_fields_from_docrules
+from forms_representator import render_fields_from_docrules
+from forms_representator import get_mdts_for_docrule
 
 
 def initDocumentIndexForm(request):
@@ -77,58 +78,9 @@ def processDocumentIndexForm(request):
         else:
             return None
 
-
-def convert_search_res(search_res, match_len):
-    docs_list = {}
-    matched_docs = []
-    for row in search_res:
-        if row.get_id in docs_list.keys():
-            docs_list[row.get_id] += 1
-        else:
-            docs_list[row.get_id] = 1
-    for doc_id, mention_count in docs_list.iteritems():
-        if int(mention_count) >= int(match_len):
-            matched_docs.append(doc_id)
-    return matched_docs
-
-
-def convert_to_search_keys(document_keys, docrule_id):
-    req_params = []
-    for key, value in document_keys.iteritems():
-        if key != "date":
-            if not "date" in document_keys.keys():
-                req_params.append([key, value, docrule_id],)
-            else:
-                req_params.append([key, value, docrule_id, str_date_to_couch(document_keys["date"])],)
-    return req_params
-
-
-def cleanup_document_keys(document_keys):
-    # cleaning up key/value pairs that have empty values from couchdb search request
-    del_list = []
-    for key, value in document_keys.iteritems():
-        if not value:
-            del_list.append(key)
-    for key in del_list:
-        del document_keys[key]
-    return document_keys
-
-
-def str_date_to_couch(from_date):
-    """
-    Converts date from form date widget generated format, like '2012-03-02'
-    To CouchDocument stored date. E.g.: '2012-03-02T00:00:00Z'
-    """
-    # TODO: HACK: normal datetime conversions here
-    couch_date = from_date + 'T00:00:00Z'
-#    date = datetime.datetime.strptime(from_date, "%Y-%m-%d")
-#    couch_date = datetime.datetime.now()
-    return couch_date
-
-
 def get_mdts_for_documents(documents):
     """
-
+    Returns list of mdts for provided documents list
     """
     indexes = {}
     resp = None
@@ -183,7 +135,7 @@ def cleanup_indexing_session(request):
     except KeyError:
         pass
 
-def cleanup_mds(request):
+def cleanup_mdts(request):
     """
     Cleanup MDT's in improvised cache.
     """
