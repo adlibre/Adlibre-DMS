bf4df484926dfd500b565e4fe7dd0d2a0d7723e8
Andrew Cutler <macropin@gmail.com>
2012-04-19 15:05:24 +1000

    Adjust project structure so that it can be installed using setup.py.

--- src/dms/settings.py
+++ src/dms/settings.py
@@ -26,7 +26,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, '..', '..', 'db', 'dms.sqlite'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, '..', 'db', 'dms.sqlite'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -64,9 +64,9 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'www', 'media')
-DOCUMENT_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'documents')
-FIXTURE_DIRS = ( os.path.join(PROJECT_PATH, '..', '..', 'fixtures'), )
+MEDIA_ROOT = os.path.join(PROJECT_PATH, '..', 'www', 'media')
+DOCUMENT_ROOT = os.path.join(PROJECT_PATH, '..', 'documents')
+FIXTURE_DIRS = ( os.path.join(PROJECT_PATH, '..', 'fixtures'), )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -77,7 +77,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, '..', '..', 'www', 'static')
+STATIC_ROOT = os.path.join(PROJECT_PATH, '..', 'www', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -94,7 +94,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_PATH, 'custom_static'),
-
 )
 
 # List of finder classes that know how to find static files in
