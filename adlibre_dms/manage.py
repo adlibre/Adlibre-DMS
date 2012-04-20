f20ae77f8285f70bf33785d190654ca42fe92232
Andrew Cutler <macropin@gmail.com>
2012-04-20 12:14:05 +1000

    Changed paths to reflect new src dir name is adlibre_dms

--- adlibre_dms/manage.py
+++ adlibre_dms/manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
+
+# Customised manage.py
+
 from django.core.management import execute_manager
-import imp
 try:
-    imp.find_module('settings') # Assumed to be in the same directory.
+    from adlibre_dms import settings # this will also import the local_settings.py in this directory.
 except ImportError:
     import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
+    sys.stderr.write("Error: Can't find the file 'adlibre_dms.settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
-
-import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
