7b3153f6834ed378a0f7b4b94a3e04a0dc086dae
Andrew Cutler <macropin@gmail.com>
2012-05-03 13:41:13 +1000

    Use custom status pages when FCGI server is down.

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(name='adlibre_dms',
         ('deployment', find_files('deployment', '*')),
         ('log', ['log/.gitignore']), # create empty dir
         ('www', ['www/.gitignore']), # create empty dir
+        ('www/status-files', find_files('www/status-files', '*')),
     ],
     install_requires=[
             # Core requirements
