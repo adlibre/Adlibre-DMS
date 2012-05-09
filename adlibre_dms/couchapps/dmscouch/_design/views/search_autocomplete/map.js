2ba65fc353ceff8b7d4c0b92c510e04c67eb3237
garmoncheg <garmon1@gmail.com>
2012-05-09 13:20:38 +0300

    Refs #723 MDT: Autocomplete keys are shared amongst disparate MDTs

--- adlibre_dms/couchapps/dmscouch/_design/views/search_autocomplete/map.js
+++ adlibre_dms/couchapps/dmscouch/_design/views/search_autocomplete/map.js
@@ -2,7 +2,7 @@ function(doc) {
     if (doc.doc_type == "CouchDocument") {
         for(var key in doc.mdt_indexes) {
             emit(
-                [key, doc.mdt_indexes[key]] ,  {rev: doc._rev}
+                [doc.metadata_doc_type_rule_id, key, doc.mdt_indexes[key]],  {rev: doc._rev}
             );// emit
         };// for
     }; //if doc_type
