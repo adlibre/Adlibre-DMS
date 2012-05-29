8280d0bc14602401b8d2f0379fba230469c09b5f
garmoncheg <garmon1@gmail.com>
2012-05-29 16:59:42 +0300

    Refs #744 CORE app workout

---
+++ adlibre_dms/apps/core/document_processor.py
@@ -0,0 +1,17 @@
+"""
+Module: DMS Core CRUD logic handler.
+
+Project: Adlibre DMS
+Copyright: Adlibre Pty Ltd 2011
+License: See LICENSE for license information
+Author: Iurii Garmash
+"""
+
+
+
+
+class DocumentProcessor(object):
+    def __init__(self):
+        self.errors = []
+        self.warnings = []
+
