39976447be17fa35c46e21e2242a4dcd740ae2f1
Andrew Cutler <macropin@gmail.com>
2012-05-29 13:45:25 +1000

    Change end date to 'creation date to' closes mikes issue 85

--- adlibre_dms/apps/mdtui/forms.py
+++ adlibre_dms/apps/mdtui/forms.py
@@ -122,7 +122,7 @@ class DocumentSearchOptionsForm(forms.Form):
     Has dynamic initialization abilities.
     """
     date = forms.DateField(label="Creation Date From", help_text="Date of the document added")
-    end_date = forms.DateField(required=False, label="To", help_text="Final possible document date")
+    end_date = forms.DateField(required=False, label="Creation Date To", help_text="Final possible document date")
 
     def __init__(self, *args, **kwargs):
         super(DocumentSearchOptionsForm, self).__init__(*args, **kwargs)
