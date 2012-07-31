5f4a9df6e2d283280e154873e6ab677e465d72ba
garmoncheg <garmon1@gmail.com>
2012-07-31 11:08:16 +0300

    Refs #794 MUI: parallel keys view (for typeahead) is too slow (final single key suggestion)

--- adlibre_dms/couchapps/dmscouch/_design/views/search_autocomplete/reduce.js
+++ adlibre_dms/couchapps/dmscouch/_design/views/search_autocomplete/reduce.js
@@ -1,13 +1,3 @@
 function (key, values, rereduce) {
-    var o = {}, i, l = values.length, r = [];
-
-    for (i = 0; i < l; i += 1) {
-        o[values[i]] = values[i];
-    }
-
-    for (i in o) {
-        r.push(o[i]);
-    }
-
-    return r;
+    return 1;
 }
