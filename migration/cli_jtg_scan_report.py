f9997760295b62b91bfbeec69d5a71b5ab6995ed
garmoncheg <garmon1@gmail.com>
2012-08-06 16:02:40 +0300

    Added hash to script

--- migration/cli_jtg_scan_report.py
+++ migration/cli_jtg_scan_report.py
@@ -6,10 +6,49 @@ A simple report to show all the documents in our DMS
 - MD5 sum of the latest revision (find duplicates / holding images)
 """
 
-import httplib, json, sys
+import json, sys, urllib2
+import hashlib
+import datetime
 
 from lib_couch import prettyPrint, Couch
 
+# DMS API parameters
+host = 'http://127.0.0.1:8000/'
+api_url = 'api/file/'
+username = 'admin'
+password = 'admin'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT) DMS Client'
+
+def get_api_file(filename, parsable, revision=None):
+    """
+    Retrieves file from DMS API, using DMS simple auth.
+    requires (variables and their example value):
+
+        host = 'http://127.0.0.1:8000/'
+        api_url = 'api/file/'
+        username = 'admin'
+        password = 'admin'
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT) DMS Client'
+    """
+    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, host, username, password)
+    handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+    opener = urllib2.build_opener(handler)
+    urllib2.install_opener(opener)
+
+    if revision:
+        request = urllib2.Request(host+api_url+filename+'?revision='+revision)
+    else:
+        request = urllib2.Request(host+api_url+filename)
+    request.add_header('User-agent', user_agent)
+    response = None
+    try:
+        response = opener.open(request)
+    except urllib2.HTTPError, e:
+        if not parsable:
+            print e
+        pass
+    return response.read()
 
 def show_doc_report(parsable=False):
     """
@@ -42,17 +81,28 @@ def show_doc_report(parsable=False):
         # Get revisions
         if revisions:
             revision_names = [rev_name for rev_name in revisions.iterkeys()]
+            revision_names.sort()
         else:
             revision_names = []
 
+        latest_revision = ''
+        if revision_names:
+            latest_revision = revision_names[revision_names.__len__() - 1]
+
         # Get MD5 sum of the latest document revision
-        # TODO
+        h_code=''
+        if latest_revision:
+            api_file = get_api_file(bar_code, parsable, latest_revision)
+
+            h = hashlib.new('md5')
+            h.update(api_file)
+            h_code = h.hexdigest()
 
         # Format the output
         if parsable:
-            print '"%s", "%s", "%s"' % (bar_code, revisions.__len__(), employee)
+            print '"%s", "%s", "%s", "%s"' % (bar_code, revisions.__len__(), employee, h_code)
         else:
-            print '%s, revision count: %s, names: %s, employee: %s' % (bar_code, revisions.__len__(), str(revision_names), employee)
+            print '%s, revision count: %s, names: %s, employee: %s, hashcode: %s' % (bar_code, revisions.__len__(), str(revision_names), employee, h_code)
 
 if __name__ == "__main__":
     """
@@ -71,4 +121,8 @@ if __name__ == "__main__":
     if sys.argv.__len__() > 1:
         if '-p' or '--parsable' in sys.argv:
             parsable = True
+    t1 = datetime.datetime.now()
     show_doc_report(parsable)
+    t2 = datetime.datetime.now()
+    if not parsable:
+        print 'Execution time: %s' % (t2-t1)
