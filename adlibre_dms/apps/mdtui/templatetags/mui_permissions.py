566a92ba6cfe70d62d3e32430e7d792f174127b2
garmoncheg <garmon1@gmail.com>
2012-08-27 10:25:17 +0300

    Refs #764 MUI: Edit Metadata (feature almost done)

--- adlibre_dms/apps/mdtui/templatetags/mui_permissions.py
+++ adlibre_dms/apps/mdtui/templatetags/mui_permissions.py
@@ -59,3 +59,27 @@ def check_index_permit(context):
         permission = True
     context['index_permitted'] = permission
     return ''
+
+@register.simple_tag(takes_context=True)
+def check_index_permit(context):
+    """
+    Checks request.user for permission to EDIT DOCUMENT INDEX in MUI
+
+    In fact he must be in search group in security.
+    Set's up context variable 'index_permitted'
+    it can be used farther in IF template compassion.
+    """
+    # Do nothing if context variable has already been set
+    if 'edit_index_permitted' in context:
+        return ''
+    user = context['request'].user
+    permission = False
+    if not user.is_superuser:
+        groups = user.groups.all()
+        for group in groups:
+            if group.name == SEC_GROUP_NAMES['edit_index']:
+                permission = True
+    else:
+        permission = True
+    context['edit_index_permitted'] = permission
+    return ''
