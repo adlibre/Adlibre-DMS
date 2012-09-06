8639de5544b67424f10a69561f586f8a4aa0e08d
garmoncheg <garmon1@gmail.com>
2012-09-06 20:42:03 +0300

    Refs #727 MDTUI: File Upload Status

--- adlibre_dms/settings.py
+++ adlibre_dms/settings.py
@@ -227,6 +227,13 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+        }
+}
+
 import logging
 class NoMessageFailuresFilter(logging.Filter):
     """
