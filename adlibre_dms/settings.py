08f985062d62fec914700e678ff6f3b36e772359
Iurii Garmash <garmon1@gmail.com>
2012-10-02 22:52:10 +0300

    log usage fix

--- adlibre_dms/settings.py
+++ adlibre_dms/settings.py
@@ -381,7 +381,7 @@ if DEBUG:
 LOGIN_REDIRECT_URL = '/'
 
 # django-log-file-viewer setttings
-LOG_FILES_DIR = os.path.join(PROJECT_PATH, '..', 'log/')
+LOG_FILES_DIR = os.path.join(PROJECT_PATH, '..', 'log')
 LOG_FILES_RE = '(?P<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})\s\[(?P<type>[A-Z]+)\]\s(?P<message>.+)'
 LOG_FILES_PAGINATE_LINES = 20
 
