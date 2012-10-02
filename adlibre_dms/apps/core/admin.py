1102b39ffd711365a94429c6b3e07b54949f9c9e
Iurii Garmash <garmon1@gmail.com>
2012-10-02 23:06:55 +0300

    Sessions fix

--- adlibre_dms/apps/core/admin.py
+++ adlibre_dms/apps/core/admin.py
@@ -16,12 +16,17 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
 
+def delete_without_warning(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.delete()
+delete_without_warning.short_description = "Delete Sessions without warning"
+
 class SessionAdmin(admin.ModelAdmin):
     def _session_data(self, obj):
         return obj.get_decoded()
     list_display = ['session_key', '_session_data', 'expire_date']
+    actions = [delete_without_warning]
 admin.site.register(Session, SessionAdmin)
-
 
 UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',)
 
