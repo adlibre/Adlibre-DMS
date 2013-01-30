88d4a215ac1c4ea619a625faf9a1e2d5e763cbf0
Iurii Garmash <garmon1@gmail.com>
2013-01-30 20:38:30 +0200

    garbage cleanup

--- adlibre_dms/apps/core/document_manager.py
+++
@@ -1,9 +0,0 @@
-"""
-Module: DMS Core Document manipulations handler.
-
-Project: Adlibre DMS
-Copyright: Adlibre Pty Ltd 2011
-License: See LICENSE for license information
-Author: Iurii Garmash
-"""
-
