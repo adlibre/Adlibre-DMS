b96087dbda9b8469d0c3db3f367b28ee22181abd
Iurii Garmash <garmon1@gmail.com>
2013-04-22 21:29:36 +0300

    New manage.py due to deprecations in Django 1.4

--- adlibre_dms/manage.py
+++ adlibre_dms/manage.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-
-# Customised manage.py
-
-from django.core.management import execute_manager
-try:
-    from adlibre_dms import settings # this will also import the local_settings.py in this directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'adlibre_dms.settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+import os
+import sys
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adlibre_dms.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
