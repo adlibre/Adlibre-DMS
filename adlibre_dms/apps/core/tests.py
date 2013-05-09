e39feaf260ec0e161761d84c50407b0e59bec392
Iurii Garmash <garmon1@gmail.com>
2013-05-09 20:44:57 +0300

    Refs #1050 Core manager testing requirement

---
+++ adlibre_dms/apps/core/tests.py
@@ -0,0 +1,21 @@
+"""
+Module: DMS Core tests
+
+Project: Adlibre DMS
+Copyright: Adlibre Pty Ltd 2013
+License: See LICENSE for license information
+Author: Iurii Garmash
+"""
+
+from adlibre.dms.base_test import DMSTestCase
+from django.test import TestCase
+
+
+class CoreTestCase(DMSTestCase):
+    """Data for testing Core specific workflows"""
+    pass
+
+
+class DocumentProcessorTest(CoreTestCase):
+    """Test for core.DocumentProcessor() and it's workflows"""
+    pass
