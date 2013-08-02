d6de0dcb63378191fc44046f5bee9b9249cef8a0
Iurii Garmash <garmon1@gmail.com>
2013-08-02 17:51:19 +0300

    PEP 8

--- adlibre_dms/apps/dms_plugins/workers/validators/filetype.py
+++ adlibre_dms/apps/dms_plugins/workers/validators/filetype.py
@@ -1,45 +1,42 @@
-import magic
+"""
+Module: File Type Plugin
+Project: Adlibre DMS
+Copyright: Adlibre Pty Ltd 2013
+License: See LICENSE for license information
+"""
 
 from dms_plugins.pluginpoints import BeforeStoragePluginPoint
 from dms_plugins.workers import Plugin, PluginError
 
-MIMETYPES = [
-                ('application/pdf', 'pdf'),
-                ('image/tiff', 'tiff'),
-                ('image/jpeg', 'jpg'),
-                ('image/gif', 'gif'),
-                ('image/png', 'png'),
-                ('text/plain', 'txt'),
-                ('text/x-c', 'txt'),  # In fact is a txt file type
-                ('text/x-c++', 'txt'),  # In fact is a txt file type
-                ('application/msword', 'doc'),
-                ('application/vnd.ms-excel', 'xls'),
-            ]
-
-def get_mimetypes():
-    return [x[0] for x in MIMETYPES]
 
 class FileTypeValidationPlugin(Plugin, BeforeStoragePluginPoint):
     title = "File Type Validator"
     description = "Validates document type against supported types"
     plugin_type = "storage_validation"
 
-    mimetypes = get_mimetypes()
+    def work(self, document):
+        """Main method to validate a MIME type of a file
 
-    def work(self, document, **kwargs):
+        @param document: DMS Document() instance"""
         if document.get_file_obj():
-            # Commented to support 0 revisions doc storage
-            # filebuffer = document.get_file_obj()
-            # if filebuffer is None:
-            #     raise PluginError('File buffer not initialized', 500)
-            # TODO: FIXME: This does not actually validate the mimetype! filebuffer is not actually used!
-            #mime = magic.Magic( mime = True )
-            #content = ''
-            #for line in filebuffer:
-            #    content += line
-            #typ = mime.from_buffer( content )
             typ = document.get_mimetype()
-            if not typ in self.mimetypes:
+            if not typ in self.get_mime_types():
                 raise PluginError('File type %s is not supported' % typ, 500)
             document.set_mimetype(typ)
         return document
+
+    def get_mime_types(self):
+        """Method to return all the file MIME types DMS supports"""
+        MIMETYPES = [
+            ("application/pdf", 'pdf'),
+            ('image/tiff', 'tiff'),
+            ('image/jpeg', 'jpg'),
+            ('image/gif', 'gif'),
+            ('image/png', 'png'),
+            ('text/plain', 'txt'),
+            ('text/x-c', 'txt'),  # In fact is a txt file type
+            ('text/x-c++', 'txt'),  # In fact is a txt file type
+            ('application/msword', 'doc'),
+            ('application/vnd.ms-excel', 'xls'),
+        ]
+        return [x[0] for x in MIMETYPES]
