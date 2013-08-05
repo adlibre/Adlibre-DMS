a615b26bf9f5f43349fa1f743afa985b64083000
Iurii Garmash <garmon1@gmail.com>
2013-08-05 12:19:53 +0300

    PEP 8

--- adlibre_dms/apps/dms_plugins/models.py
+++ adlibre_dms/apps/dms_plugins/models.py
@@ -9,7 +9,7 @@ License: See LICENSE for license information
 from django.db import models
 import logging
 
-from djangoplugins.fields import PluginField, ManyPluginField
+from djangoplugins.fields import ManyPluginField
 from djangoplugins.models import Plugin
 from taggit.managers import TaggableManager
 
@@ -18,47 +18,67 @@ from doc_codes.models import DocumentTypeRule
 
 log = logging.getLogger('dms_plugins.models')
 
+
 class DoccodePluginMapping(models.Model):
+    """A Relational storage for handling DocumentType <=> Plugins relations"""
+
     doccode = models.ForeignKey(DocumentTypeRule)
     #if changing *_plugins names please change dms_plugins.pluginpoints correspondingly
-    before_storage_plugins = ManyPluginField(   pluginpoints.BeforeStoragePluginPoint, 
-                                                related_name = 'settings_before_storage',
-                                                blank = True,
-                                                verbose_name = "Pre-Storage Workflow")
-    database_storage_plugins = ManyPluginField(   pluginpoints.DatabaseStoragePluginPoint,
-                                                related_name = 'settings_database_storage',
-                                                blank = True,
-                                                verbose_name = "Database Storage Workflow")
-    storage_plugins = ManyPluginField(   pluginpoints.StoragePluginPoint, 
-                                                related_name = 'settings_storage',
-                                                blank = True,
-                                                verbose_name = "Storage Workflow")
-    before_retrieval_plugins = ManyPluginField( pluginpoints.BeforeRetrievalPluginPoint, 
-                                                related_name = 'settings_before_retrieval',
-                                                blank = True,
-                                                verbose_name = "Retrieval Workflow")
-    before_removal_plugins = ManyPluginField( pluginpoints.BeforeRemovalPluginPoint, 
-                                                related_name = 'settings_before_removal',
-                                                blank = True,
-                                                verbose_name = "Removal Workflow")
-    before_update_plugins = ManyPluginField( pluginpoints.BeforeUpdatePluginPoint, 
-                                                related_name = 'settings_before_update',
-                                                blank = True,
-                                                verbose_name = "Pre-Modification Workflow")
-    update_plugins = ManyPluginField( pluginpoints.UpdatePluginPoint,
-                                             related_name = 'settings_update',
-                                             blank = True,
-                                             verbose_name = "Modification Workflow")
-    database_update_plugins = ManyPluginField( pluginpoints.DatabaseUpdatePluginPoint,
-                                             related_name = 'settings_database_update',
-                                             blank = True,
-                                             verbose_name = "Database Modification Workflow")
-    active = models.BooleanField(default = False)
+    before_storage_plugins = ManyPluginField(
+        pluginpoints.BeforeStoragePluginPoint,
+        related_name='settings_before_storage',
+        blank=True,
+        verbose_name="Pre-Storage Workflow"
+    )
+    database_storage_plugins = ManyPluginField(
+        pluginpoints.DatabaseStoragePluginPoint,
+        related_name='settings_database_storage',
+        blank=True,
+        verbose_name="Database Storage Workflow"
+    )
+    storage_plugins = ManyPluginField(
+        pluginpoints.StoragePluginPoint,
+        related_name='settings_storage',
+        blank=True,
+        verbose_name="Storage Workflow"
+    )
+    before_retrieval_plugins = ManyPluginField(
+        pluginpoints.BeforeRetrievalPluginPoint,
+        related_name='settings_before_retrieval',
+        blank=True,
+        verbose_name="Retrieval Workflow"
+    )
+    before_removal_plugins = ManyPluginField(
+        pluginpoints.BeforeRemovalPluginPoint,
+        related_name='settings_before_removal',
+        blank=True,
+        verbose_name="Removal Workflow"
+    )
+    before_update_plugins = ManyPluginField(
+        pluginpoints.BeforeUpdatePluginPoint,
+        related_name='settings_before_update',
+        blank=True,
+        verbose_name="Pre-Modification Workflow"
+    )
+    update_plugins = ManyPluginField(
+        pluginpoints.UpdatePluginPoint,
+        related_name='settings_update',
+        blank=True,
+        verbose_name="Modification Workflow"
+    )
+    database_update_plugins = ManyPluginField(
+        pluginpoints.DatabaseUpdatePluginPoint,
+        related_name='settings_database_update',
+        blank=True,
+        verbose_name="Database Modification Workflow"
+    )
+    active = models.BooleanField(default=False)
 
     def __unicode__(self):
         return unicode(self.get_name())
 
     def get_name(self):
+        """Returns a name of this instance"""
         try:
             doccode_name = self.doccode.get_title()
         except (KeyError, AttributeError):
@@ -68,6 +88,7 @@ class DoccodePluginMapping(models.Model):
     name = property(get_name)
 
     def get_docrule(self):
+        """Returns this instance's DocumentType"""
         return self.doccode
 
     def get_before_storage_plugins(self):
@@ -118,20 +139,21 @@ class DoccodePluginMapping(models.Model):
         """
         return self.database_storage_plugins.all().order_by('index')
 
+
 class PluginOption(models.Model):
+    """An option for a Plugin() object"""
     pluginmapping = models.ForeignKey(DoccodePluginMapping)
     plugin = models.ForeignKey(Plugin)
-    name = models.CharField(max_length = 64)
-    value = models.TextField(default = '', blank = True)
+    name = models.CharField(max_length=64)
+    value = models.TextField(default='', blank=True)
 
     def __unicode__(self):
         return "%s: %s" % (self.name, self.value)
 
+
 class DocTags(models.Model):
-    """
-        A model that represents Document for maintainig Tag relations.
-    """
-    name = models.CharField(max_length = 128)
+    """A model that represents Document for maintaining Tag relations."""
+    name = models.CharField(max_length=128)
     doccode = models.ForeignKey(DocumentTypeRule)
     tags = TaggableManager()
 
@@ -140,6 +162,7 @@ class DocTags(models.Model):
         verbose_name_plural = "Document > Tags mappings"
 
     def get_tag_list(self):
+        """Returns a list of current tags"""
         return map(lambda x: x.name, self.tags.all())
 
     def __unicode__(self):
