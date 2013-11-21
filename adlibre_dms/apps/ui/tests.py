dedf86f2ddf2dcc9e78ecd4b21d272a5885e8256
Iurii Garmash <garmon1@gmail.com>
2013-11-21 19:14:28 +0200

    Refs #1294 AUI: DMS support of Unclassified files uploads (No doccode). Tidy up UI app tests.

--- adlibre_dms/apps/ui/tests.py
+++ adlibre_dms/apps/ui/tests.py
@@ -1,16 +1,12 @@
 """
-Module: DMS UI Django Unit Tests
+Module: DMS UI App Django Unit Tests
+
 Project: Adlibre DMS
 Copyright: Adlibre Pty Ltd 2011
 License: See LICENSE for license information
 Author: Iurii Garmash
 """
 
-"""
-Basic UI tests
-"""
-
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from adlibre.dms.base_test import DMSTestCase
 
@@ -32,13 +28,6 @@ regular_documents = [
     'abcde888.pdf',
     'abcde999.pdf',
 ]
-
-#nodoccode_documents = [
-#    'ADL-12345.pdf',
-#    'ADL-54321.pdf',
-#    'test_document_template.odt',
-#    'test_no_doccode.pdf',
-#]
 
 upload_form_html = """<form id="ui_upload_file_form" enctype="multipart/form-data" action="/ui/upload-document/" method="post">"""
 
@@ -63,12 +52,6 @@ class Main_DMS_UI_Test(DMSTestCase):
             self.assertEqual(response.status_code, 200)
             self.assertContains(response, upload_form_html)
 
-#        # Test upload of test documents, covering all No Doc Code Type Rules
-#        for filename in nodoccode_documents:
-#            response = self._ui_upload(filename)
-#            self.assertEqual(response.status_code, 200)
-#            self.assertContains(response, upload_form_html)
-
     def test_zz_cleaup(self):
         # Name of this test should be alphabetically last
         # to be ran after all tests finished
@@ -80,11 +63,3 @@ class Main_DMS_UI_Test(DMSTestCase):
             response = self.client.delete(url)
             self.assertEqual(response.status_code, 204)
 
-#        # Test delete of test documents, covering all No Doc Code Type Rules
-#        for doc in nodoccode_documents:
-#            code, suggested_format = os.path.splitext(doc)
-#            url = reverse('api_file', kwargs={'code': code,})
-#            from datetime import date # Hack
-#            data = {'parent_directory': str(date.today()), 'full_filename': doc, } # hack
-#            response = self.client.delete(url, data)
-#            self.assertEqual(response.status_code, 204)
