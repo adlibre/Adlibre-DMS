0582981b941d940efd47cd53a89f6b5050b5e9a1
garmoncheg <yuri@adlibre.com.au>
2014-02-05 17:32:00 +0200

    admin help list display

--- adlibre_dms/apps/dms_plugins/admin.py
+++ adlibre_dms/apps/dms_plugins/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from dms_plugins.models import DoccodePluginMapping
 
-admin.site.register(DoccodePluginMapping)
+
+class DoccodePluginMappingAdmin(admin.ModelAdmin):
+    list_display = ('doccode', 'active', )
+
+admin.site.register(DoccodePluginMapping, DoccodePluginMappingAdmin)
