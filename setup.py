9a5650b5bf2a369b44987e067a11705381615412
garmoncheg <yuri@adlibre.com.au>
2015-02-16 12:31:50 +0200

    Update BCP version used.
    1.1.8 is unavailable now, using current 0.2.2 instead

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(name='adlibre_dms',
             # Tagging
             'django-taggit==0.11.2',
             # Adlibre Components
-            'django-bcp==0.1.8',
+            'django-bcp==0.2.2',
             # Plugins
             'django-plugins',
             # Deployment
